273ebcaa435d273694ab850971d50859aed47835
Omar Alzare <alzar3omar@gmail.com>
2024-11-15 11:52:11 +0300

    Organizing files

--- Day 1/Exercise3.py
+++ Day 1/Exercise3.py
@@ -1,1 +1,13 @@
-
+# Exercise 3 : Variables
+# We have 2 variables glass1 and glass2.
+# glass1 contains milk and glass2 contains juice.
+# Write 3 lines of code to switch the contents of the variables.
+# You are not allowed to type the words "milk" or "juice".
+# You are only allowed to use variables to solve this exercise.
+glass1 = "milk"
+glass2 = "juice"
+tempGlass = glass1
+glass1 = glass2
+glass2 = tempGlass
+# Extra line of code
+print(glass1 + " " + glass2)
