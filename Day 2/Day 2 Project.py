9f960fcead347d13ad5e16fe347519e1f3354309
Omar Alzare <alzar3omar@gmail.com>
2024-11-16 00:52:48 +0300

    adding to project 2

--- Day 2/Day 2 Project.py
+++ Day 2/Day 2 Project.py
@@ -1,8 +1,8 @@
 # https://appbrewery.github.io/python-day2-demo/
 
 print("Welcome to the tip calculator!")
-bill = int(input("What was the total bill? $"))
-tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
-realTip = tip / 100
-numOfPeople = int(input("How many people to split the bill? "))
-print(f"Each person should pay: ${(bill + (realTip * bill)) / numOfPeople}")
+bill = float(input("What was the total bill? $"))
+tip = float(input("How much tip would you like to give? 10, 12, or 15? ")) / 100
+numOfPeople = float(input("How many people to split the bill? "))
+result = round((bill + (tip * bill)) / numOfPeople, 2)
+print(f"Each person should pay: ${result}")
