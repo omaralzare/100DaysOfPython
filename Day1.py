1c029db4a3f2b13c8401dc7819a361f9a8f8711a
Omar Alzare <alzar3omar@gmail.com>
2024-11-15 08:31:33 +0300

    Day 1

---
+++ Day1.py
@@ -0,0 +1,6 @@
+# https://appbrewery.github.io/python-day1-demo/
+
+print("Welcome to the Band Name Generator.")
+cityName = str(input("What's the name of the city you grew up in?\n"))
+petName = str(input("What's your pet's name?\n"))
+print("Your band name could be", cityName, petName)
