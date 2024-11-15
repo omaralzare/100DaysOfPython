fad21d71542e8c463cf0d77fbe6bd897a83ca5e6
Omar Alzare <alzar3omar@gmail.com>
2024-11-15 17:24:16 +0300

    edit project 2

--- Day 2/Day 2 Project.py
+++ Day 2/Day 2 Project.py
@@ -5,4 +5,4 @@ bill = int(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 realTip = tip / 100
 numOfPeople = int(input("How many people to split the bill? "))
-print(f"Each person should pay: ${bill + (realTip * bill) / numOfPeople}")
+print(f"Each person should pay: ${(bill + (realTip * bill)) / numOfPeople}")
